58a7236a4547df295cd1db59505ab471421ee7dc
Romanovskyi Ivan <romanovskijv508@gmail.com>
2026-08-23 02:01:28 +0200

    feat: make import username configurable

--- src/main/java/ch/iselaj/quarry/QuarryApplication.java
+++ src/main/java/ch/iselaj/quarry/QuarryApplication.java
@@ -2,10 +2,12 @@ package ch.iselaj.quarry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 import ch.iselaj.quarry.game.GameImportService;
 import ch.iselaj.quarry.source.GameSource;
@@ -21,7 +23,10 @@ public class QuarryApplication {
 
 
 	@Bean
-		ApplicationRunner smokeTest(GameImportService importService) {
-    		return args -> importService.importGames("HappySettler", 20);
-		}
+	ApplicationRunner smokeTest(
+        	GameImportService importService,
+        	@Value("${quarry.import.username}") String username,
+        	@Value("${quarry.import.max}") int max) {
+    	return args -> importService.importGames(username, max);
+	}
 }
